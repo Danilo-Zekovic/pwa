06a2c6598b943cd5b03874060a74fdb25bbb8b16
Danilo Zekovic <danilozeka93@gmail.com>
2017-04-17 13:16:01 +0200

    It is as in tutorial

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// ServiceWorker file
+var CACHE_NAME = 'my-pwa-cache-v1';
+var urlsToCache = [
+  '/',
+  '/bundle.js'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Open a cache and cache our files
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
