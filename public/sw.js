85410dd6d62c5742a36b9de7857e17884592c61b
Danilo Zekovic <danilozeka93@gmail.com>
2017-04-23 12:24:57 +0200

    testing cache update reload

--- public/sw.js
+++ public/sw.js
@@ -14,11 +14,51 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
-});
+});*/
+
+self.addEventListener('fetch', function(evt){
+  console.log('sw serving');
+
+  evt.respondWith(fromCache(evt.request))
+
+  evt.waitUntil(
+    update(evt.request).then(refresh)
+  )
+})
+
+function fromCache(request){
+  return caches.open(CACHE_NAME).then(function (cache){
+    return cache.match(request)
+  })
+}
+
+function update (request){
+  return caches.open(CACHE_NAME).then(function(cache){
+    return fetch(request).then(function(response){
+      return cache.put(request, response.clone()).then(function () {
+        return response
+      })
+    })
+  })
+}
+
+function refresh(response){
+  return self.clients.matchAll().then(function(clients){
+    clients.forEach(function (client){
+      var message = {
+        type:'refresh',
+        url:response.url,
+        eTagresponse.headers.get('ETag')
+      }
+
+      client.postMessage(JSON.stringify(message))
+    })
+  })
+}
