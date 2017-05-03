4a89812aff74731d8afdb16e60c4138495236cdc
Danilo Zekovic <danilozeka93@gmail.com>
2017-05-03 22:55:49 +0200

    fresh start

--- app/components/views/Subscribe.js
+++ app/components/views/Subscribe.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import webPush from 'web-push'
-
-console.log("<<<< HEY THIS ACTUALLY WORKS >>>>")
-// VAPID key
-const applicationServerPublicKey = 'BAJyqorVg8OWJwiRJnz7A2CDFFstpXsyt8m0P3MQPCIxx1PgREuRqt-4lyVDy26rQF0njxQGkzK4aF_sjoooFGM'
 
 let pushButton = null;
 
 let isSubscribed = false;
-let swRegistration = null
+let swRegistration = null;
 
 // Check for service worker
 if ('serviceWorker' in navigator && 'PushManager' in window) {
@@ -16,80 +11,22 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    swRegistration = swReg;
+    swRegistration = registration;
   }).catch(function(err) {
     // registration failed s
     console.log('ServiceWorker registration failed: ', err);
   });
-} else {
+}else{
   console.warn('Push messaging is not supported');
   pushButton.textContent = 'Push Not Supported';
-}
-
-//===================================================
-function askPermission() {
-  return new Promise(function(resolve, reject) {
-    const permissionResult = Notification.requestPermission(function(result) {
-      resolve(result);
-    });
-
-    if (permissionResult) {
-      permissionResult.then(resolve, reject);
-    }
-  })
-  .then(function(permissionResult) {
-    if (permissionResult !== 'granted') {
-      throw new Error('We weren\'t granted permission.');
-    }
-  });
-}
-
-function subscribeUserToPush() {
-  return getSWRegistration()
-  .then(function(registration) {
-    const subscribeOptions = {
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        'BAJyqorVg8OWJwiRJnz7A2CDFFstpXsyt8m0P3MQPCIxx1PgREuRqt-4lyVDy26rQF0njxQGkzK4aF_sjoooFGM'
-      )
-    };
-
-    return registration.pushManager.subscribe(subscribeOptions);
-  })
-  .then(function(pushSubscription) {
-    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-    return pushSubscription;
-  });
-}
-//===================================================
-//===================================================
-
-function sendSubscriptionToBackEnd(subscription) {
-  return fetch('/save-subscription/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(subscription)
-  })
-  .then(function(response) {
-    if (!response.ok) {
-      throw new Error('Bad status code from server.');
-    }
-
-    return response.json();
-  })
-  .then(function(responseData) {
-    if (!(responseData.data && responseData.data.success)) {
-      throw new Error('Bad response from server.');
-    }
-  });
 }
 
 const Subscribe = () => (
   <div >
     <h2>Subscribe PWA</h2>
-    <button>Click me</button>
+    <button disabled class="js-push-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+      Enable Push Messaging
+    </button>
   </div>
 )
 
--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,7 @@
-import webpush from 'web-push'
-import Datastore from 'nedb'
-const db = new Datastore();
-
 const vapidKeys = {
   publicKey:'BAJyqorVg8OWJwiRJnz7A2CDFFstpXsyt8m0P3MQPCIxx1PgREuRqt-4lyVDy26rQF0njxQGkzK4aF_sjoooFGM',
   privateKey: 'efx_7WzMG84FsdrU-38LzoB9WhE-U2zcWtNQiXzeGPs'
 };
-
-webpush.setVapidDetails(
-  'mailto:danilo.zekovic@danilozekovic.com',
-  vapidKeys.publicKey,
-  vapidKeys.privateKey
-);
 
 export default function ( router, server ) {
   const options = {
@@ -37,81 +27,4 @@ export default function ( router, server ) {
     console.log('Server marina chosen')
     res.sendFile('index.html', options)
   })
-
-  router.get('/save-subscription/', function (req, res) {
-    const isValidSaveRequest = (req, res) => {
-      // Check the request body has at least an endpoint.
-      if (!req.body || !req.body.endpoint) {
-        // Not a valid subscription.
-        res.status(400);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({
-          error: {
-            id: 'no-endpoint',
-            message: 'Subscription must have an endpoint.'
-          }
-        }));
-        return false;
-      }
-      return true;
-    };
-    return saveSubscriptionToDatabase(req.body)
-    .then(function(subscriptionId) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ data: { success: true } }));
-    })
-    .catch(function(err) {
-      res.status(500);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        error: {
-          id: 'unable-to-save-subscription',
-          message: 'The subscription was received but we were unable to save it to our database.'
-        }
-      }));
-    });
-
-    function saveSubscriptionToDatabase(subscription) {
-      return new Promise(function(resolve, reject) {
-        db.insert(subscription, function(err, newDoc) {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(newDoc._id);
-        });
-      });
-    };
-
-  }) // end subscribe
-
-  app.post('/trigger-push-msg/', function (req, res) {
-    return getSubscriptionsFromDatabase()
-    .then(function(subscriptions) {
-      let promiseChain = Promise.resolve();
-
-      for (let i = 0; i < subscriptions.length; i++) {
-        const subscription = subscriptions[i];
-        promiseChain = promiseChain.then(() => {
-          return triggerPushMsg(subscription, dataToSend);
-        });
-      }
-
-      return promiseChain;
-    })
-
-    const triggerPushMsg = function(subscription, dataToSend) {
-      return webpush.sendNotification(subscription, dataToSend)
-      .catch((err) => {
-        if (err.statusCode === 410) {
-          return deleteSubscriptionFromDatabase(subscription._id);
-        } else {
-          console.log('Subscription is no longer valid: ', err);
-        }
-      });
-    };
-    
-  }) //end push
-
 }
