6a8c815637c150eabca3b1fcbcea88a0593fcad5
Danilo Zekovic <danilozeka93@gmail.com>
2017-05-04 10:32:23 +0200

    This will break my code

--- app/components/views/Subscribe.js
+++ app/components/views/Subscribe.js
@@ -1,17 +1,61 @@
 import React from 'react'
 
-let pushButton = null;
+const applicationServerPublicKey = 'BAJyqorVg8OWJwiRJnz7A2CDFFstpXsyt8m0P3MQPCIxx1PgREuRqt-4lyVDy26rQF0njxQGkzK4aF_sjoooFGM'
+
+let pushButton = document.querySelector('.js-push-btn');
 
 let isSubscribed = false;
 let swRegistration = null;
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+/*function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}*/
+
+function initialiseUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    //updateBtn();
+  });
+}
 
 // Check for service worker
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log("<<<< Service Worker exists >>>>")
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    console.log('ServiceWorker registration successful with scope: ', registration);
     swRegistration = registration;
+    initialiseUI();
   }).catch(function(err) {
     // registration failed s
     console.log('ServiceWorker registration failed: ', err);
@@ -21,13 +65,45 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   pushButton.textContent = 'Push Not Supported';
 }
 
-const Subscribe = () => (
+function subscribeBtn(props) {
+  return (
+    <button disabled className="btn btn-primary js-push-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+      onClick = {props.onClick}
+      >
+      {props.value}
+    </button>
+  );
+}
+
+
+
+/*const Subscribe = () => (
   <div >
     <h2>Subscribe PWA</h2>
-    <button disabled class="js-push-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-      Enable Push Messaging
-    </button>
+    <subscribeBtn/>
   </div>
-)
+)*/
+class Subscribe extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btnText:'Enable Push Messaging'
+    };
+  }
+
+  handleClick(i) {
+    this.setState({
+      btnText:'Disable Push Messaging'
+    });
+  }
+
+  render{
+    return(
+      <div>
+        <subscribeBtn onClick={i => this.handleClick(i)}/>
+      </div>
+    )
+  }
+}
 
 export default Subscribe
