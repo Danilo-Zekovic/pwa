8170ba0fa76bcb55741d7c895eb0de87d2806d6a
Danilo Zekovic <danilozeka93@gmail.com>
2017-05-09 06:57:42 +0200

    clean up

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
-import Marina from './views/Marina'
+//import Marina from './views/Marina'
 import Home from './views/Home'
-import Setup from './views/Setup'
+//import Setup from './views/Setup'
 import Subscribe from './views/Subscribe'
+import Notifications from './views/Notifications'
 
 const Main = () => (
   <main style={{marginTop:69 + 'px'}} className='container'>
     <Switch>
       <Route exact path='/' component={Home}/>
-      <Route path='/setup' component={Setup}/>
+      <Route path='/notifications' component={Notifications}/>
       <Route path='/subscribe' component={Subscribe}/>
-      <Route path='/marina' component={Marina}/>
     </Switch>
   </main>
 )
---
+++ app/components/views/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Notifications = () => (
+  <div>
+    <h1>Send Push Notifications</h1>
+    <p>COMING SOON!</p>
+  </div>
+)
+
+export default Notifications
