d5c68c8f92d76a84573a0f34a96ddfa86a34065d
Danilo Zekovic <danilozeka93@gmail.com>
2017-05-11 16:36:16 +0200

    removed some dead code

--- app/index.js
+++ app/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
-/*const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);*/
 
 render(
-  //<App />,
   (<BrowserRouter>
     <App />
   </BrowserRouter>),
